6d39deeaf78b85026ddc3b19e2bfe7d430b45eee
Nicole Modesto <160046955+nicole-002@users.noreply.github.com>
2025-02-27 09:57:40 -0300

    Add files via upload

---
+++ aula_03/processa01.php
@@ -0,0 +1,10 @@
+<?php 
+    $nome = $_POST['cxnome'];
+    $idade = $_POST['cxidade'] ;
+
+    echo "Nome digitado: $nome <br>";
+    echo "Idade digitada: $idade <br>";
+
+?>
+
+<!-- "$_" são pseudovariáveis -->
