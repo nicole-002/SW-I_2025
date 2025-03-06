b66c55051d4de81e5c0fa42b1f180b49022c92b4
Nicole Modesto <160046955+nicole-002@users.noreply.github.com>
2025-03-06 09:12:23 -0300

    Add files via upload

---
+++ aula_4/processa_ex02.php
@@ -0,0 +1,27 @@
+<?php
+    $num1 = $_GET['cxva1'];
+    $num2 = $_GET['cxva2'];
+    $num3 = $_GET['cxva3'];
+
+    $maior = $num1;
+
+    if ($maior< $num2){
+        $maior == $num2;
+    }
+    else
+    {
+        $maior == $maior;
+    }
+
+    if ($maior< $num3)
+    {
+        $maior == $num3;
+    }
+    else
+    {
+        $maior == $maior;
+    }
+    
+    echo "O maior número é: $maior"
+
+?>
