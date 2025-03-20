6333f0548d75159a00ff391bd04165965e7f46e8
Nicole Modesto <160046955+nicole-002@users.noreply.github.com>
2025-03-20 08:48:18 -0300

    Add files via upload

--- aula_05/processa.php
+++ aula_05/processa.php
@@ -1,4 +1,16 @@
 <?php
-    
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    if ($email == 'aluno@email.com' && $senha == '1234'){
+        //vai para a página 'privada.php'
+        $nome = "Aluno";
+        header('Location: privada.php?nome='.$nome);
+    }
+    else{
+        //vai para a página 'erro.php'
+        header('Location: erro.php');
+    }
 
 ?>
