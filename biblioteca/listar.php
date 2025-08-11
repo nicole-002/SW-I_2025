3f5166f72110695e006e0a79cd6ebd0e7207dd45
Nicole Modesto <160046955+nicole-002@users.noreply.github.com>
2025-08-11 08:58:15 -0300

    Add files via upload

---
+++ biblioteca/listar.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include_once 'pedaco.php';
+    ?>
+
+    <h2>Lista de Livros Disponíveis</h2>
+
+    <div class='container'>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Genêro</th>
+                    <th scope="col">Autor</th>
+                    <th scope="col">Ano</th>
+                    <th scope="col">Páginas</th>
+                </tr>   
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>O Pequeno Príncipe</td>
+                    <td>Literatura Infantil</td>
+                    <td>Antoine de Saint-Exupéry</td>
+                    <td>1943</td>
+                    <td>96</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Dom Casmuro</td>    
+                    <td>Romance</td>
+                    <td>Machado de Assis</td>
+                    <td>1900</td>
+                    <td>338</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>@social</td>
+                    <td>@social</td>
+                    <td>@social</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <a href="index.php" type="button" class="btn btn-primary">Voltar</a>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ biblioteca/pedaco.php
@@ -0,0 +1,5 @@
+
+    <div>
+        <h1>Biblioteca</h1>
+        <h2>Nicole Modesto</h2>
+    </div>
